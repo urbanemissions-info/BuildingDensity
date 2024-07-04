5c53b007483b83aee4a6f44995383b6dd6958ba1
d-saikrishna <saikrishnadammalapati@gmail.com>
2024-07-04 14:55:32 +0530

    ADD DuckDB code

---
+++ duck.py
@@ -0,0 +1,15 @@
+import duckdb
+import numpy as np
+from tqdm import tqdm
+
+duckdb.sql('INSTALL httpfs')
+duckdb.sql('LOAD httpfs')
+duckdb.sql("FORCE INSTALL spatial FROM 'http://nightly-extensions.duckdb.org';")
+duckdb.sql('LOAD spatial')
+
+prefix = "s3://us-west-2.opendata.source.coop/vida/google-microsoft-open-buildings/geoparquet"
+partitions = "by_country"
+country_iso = "IND"
+
+output_file = 'buildings_india.geojson'
+duckdb.sql(f"COPY (SELECT ST_Centroid(ST_GeomFromWKB(geometry)) AS centroid, area_in_meters FROM parquet_scan('{prefix}/{partitions}/country_iso={country_iso}/{country_iso}.parquet')) TO '{output_file}' WITH  (FORMAT GDAL, DRIVER 'GeoJSON');")
