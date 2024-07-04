0efcd218b5ef88afe0946fa7d405803999f0c848
d-saikrishna <saikrishnadammalapati@gmail.com>
2024-07-04 19:16:37 +0530

    ADD requirements

--- duck.py
+++ duck.py
@@ -9,7 +9,7 @@ duckdb.sql('LOAD spatial')
 
 prefix = "s3://us-west-2.opendata.source.coop/vida/google-microsoft-open-buildings/geoparquet"
 partitions = "by_country"
-country_iso = "IND"
+country_iso = "BTN"
 
-output_file = 'buildings_india.geojson'
+output_file = 'buildings_BHUTAN.geojson'
 duckdb.sql(f"COPY (SELECT ST_Centroid(ST_GeomFromWKB(geometry)) AS centroid, area_in_meters FROM parquet_scan('{prefix}/{partitions}/country_iso={country_iso}/{country_iso}.parquet')) TO '{output_file}' WITH  (FORMAT GDAL, DRIVER 'GeoJSON');")
